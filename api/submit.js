1c8cf98d0c12f64e0e450429f8117aefda6809b5
nitturkaryash <nitturkaryash@gmail.com>
2026-09-17 19:53:22 +0000

    chore: refresh bore.pub submit proxy port

--- api/submit.js
+++ api/submit.js
@@ -4,7 +4,7 @@
  */
 const PROXY_URL =
   process.env.SUBMIT_PROXY_URL ||
-  'http://bore.pub:2450/api/submit'
+  'http://bore.pub:61363/api/submit'
 const SHEETS_URL = process.env.SHEETS_WEBHOOK_URL || ''
 const REPO = process.env.GITHUB_REPO || 'nitturkaryash/kpcl-nps-feedback'
 const BRANCH = process.env.GITHUB_BRANCH || 'main'
